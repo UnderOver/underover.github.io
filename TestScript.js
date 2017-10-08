2371a90811700d2a698614907544def062464415
UnderOver <mackenzie.dickinson01@gmail.com>
2017-10-08 23:49:04 +0100

    blah

--- TestScript.js
+++ TestScript.js
@@ -1,8 +1,15 @@
 function StartUp() {
     $('#HomeContent').fadeIn();
+    $('#HomeButton').addClass("Active-Button");
 }
 
-function TestFunction(div) {
+function ChangeMainContent(div, button) {
     $('.innerContent').hide();
     $('#' + div).fadeIn();
+    $('.Content-Button').removeClass("Active-Button");
+    $('#' + button).addClass('Active-Button');
 };
+
+function ShowNavBox() {
+    $('.NavBox').toggleClass('showing');
+}
